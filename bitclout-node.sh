faa9b7a64613192f90f7b4d8ba98da5b9a3eeec5
Sean <sean@rartoo.com>
2021-06-25 06:57:01 +0930

    bitclout-node.sh

---
+++ bitclout-node.sh
@@ -0,0 +1,46 @@
+sudo apt-get update
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+sudo apt-get install unzip
+
+wget https://github.com/bitclout/run/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cd run-main
+
+sudo cp docker-compose.dev.yml docker-compose.yml
+
+sudo nano dev.env
+
+sudo apt-get upgrade
+
+sudo apt-get update
