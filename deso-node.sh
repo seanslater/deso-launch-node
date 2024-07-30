907e4b3f1cb1b5bc7b61bfaf10fd8e8bcd210a10
Sean <sean@rartoo.com>
2024-07-30 20:16:52 +0930

    Update deso-node.sh

--- deso-node.sh
+++ deso-node.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install build-essential unzip ca-certificates curl
 git clone https://github.com/deso-protocol/run.git
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -9,8 +9,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
-
 echo "Installation Complete."
